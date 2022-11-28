84d4de5ed83c69d63f04e6a019db98eee13898ee
Stanley Fujimoto <sfujimoto@ancestry.com>
2022-11-28 08:41:21 -0700

    update for zsh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-cp .vimrc .bashrc .bash_profile ~/
+sudo yum install zsh util-linus-user
+
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+cp .vimrc .zshrc ~/
 
 git config --global user.name "M. Stanley Fujimoto"
 git config --global user.email sfujimoto@gmail.com
