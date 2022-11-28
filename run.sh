1e400bb78fed1cd1408d384d037601e0516fdaa5
M. Stanley Fujimoto <sfujimoto@gmail.com>
2022-11-28 11:00:11 -0700

    change password

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
-sudo yum install zsh util-linus-user
+sudo yum install zsh util-linux-user -y
 
+rm -rf ~/.oh-my-zsh
+
+command -v zsh | sudo tee -a /etc/shells
+sudo passwd ec2-user
 chsh -s $(which zsh)
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
